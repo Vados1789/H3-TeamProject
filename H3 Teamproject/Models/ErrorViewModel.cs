291e526fff659a4c4586c62800ecb9f25ed88f1c
Vados1789 <42692647+Vados1789@users.noreply.github.com>
2023-08-08 11:20:35 +0200

    Copy project

---
+++ H3 Teamproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace H3_Teamproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
